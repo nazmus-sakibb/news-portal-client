d011408c35dc7706b907214baf84a1484b186fab
NAZMUS SAKIB <sakibabir52@gmail.com>
2021-12-15 02:48:42 +0600

    deploying

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "https://news-portal2.herokuapp.com/api/"
+    baseURL: "https://news-portal2.herokuapp.com/api"
 });
